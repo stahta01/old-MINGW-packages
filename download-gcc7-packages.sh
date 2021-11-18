ef9ebf1d73451f317e145f723a4a9239e10b8890
Tim Stahlhut <stahta01@gmail.com>
2021-11-17 22:39:16 -0500

    script: Add download-gcc7-packages.sh

---
+++ download-gcc7-packages.sh
@@ -0,0 +1,11 @@
+mkdir -p packages && cd packages
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC7_alpha_2/mingw-w64-i686-crt-git-6.0.0.5241.e174bf62d-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC7_alpha_2/mingw-w64-i686-gcc7-7.5.0-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC7_alpha_2/mingw-w64-i686-gcc7-libs-7.5.0-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC7_alpha_2/mingw-w64-i686-headers-git-6.0.0.5241.e174bf62d-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC7_alpha_2/mingw-w64-i686-pthread-stub-headers-git-6.0.0.5241.e174bf62d-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC7_alpha_2/mingw-w64-x86_64-crt-git-6.0.0.5241.e174bf62d-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC7_alpha_2/mingw-w64-x86_64-gcc7-7.5.0-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC7_alpha_2/mingw-w64-x86_64-gcc7-libs-7.5.0-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC7_alpha_2/mingw-w64-x86_64-headers-git-6.0.0.5241.e174bf62d-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC7_alpha_2/mingw-w64-x86_64-pthread-stub-headers-git-6.0.0.5241.e174bf62d-2-any.pkg.tar.zst
