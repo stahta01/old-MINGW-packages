9ed7ace1521353e08bd812228fc33311430f9c88
Tim Stahlhut <stahta01@gmail.com>
2021-11-21 08:35:11 -0500

    script: Add download-gcc4.7-packages.sh

---
+++ download-gcc4.7-packages.sh
@@ -0,0 +1,11 @@
+mkdir -p packages && cd packages
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC4.7_alpha_2/mingw-w64-i686-crt3-git-3.4.0.3935.8c7e00886-1-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC4.7_alpha_2/mingw-w64-i686-gcc4.7-4.7.4-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC4.7_alpha_2/mingw-w64-i686-gcc4.7-libs-4.7.4-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC4.7_alpha_2/mingw-w64-i686-headers3-git-3.4.0.3935.8c7e00886-1-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC4.7_alpha_2/mingw-w64-i686-pthread-stub-headers3-git-3.4.0.3935.8c7e00886-1-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC4.7_alpha_2/mingw-w64-x86_64-crt3-git-3.4.0.3935.8c7e00886-1-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC4.7_alpha_2/mingw-w64-x86_64-gcc4.7-4.7.4-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC4.7_alpha_2/mingw-w64-x86_64-gcc4.7-libs-4.7.4-2-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC4.7_alpha_2/mingw-w64-x86_64-headers3-git-3.4.0.3935.8c7e00886-1-any.pkg.tar.zst
+wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/GCC4.7_alpha_2/mingw-w64-x86_64-pthread-stub-headers3-git-3.4.0.3935.8c7e00886-1-any.pkg.tar.zst
