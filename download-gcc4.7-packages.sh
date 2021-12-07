68fa1d73061caec3a5803f66281e71baf4c14e18
Tim Stahlhut <stahta01@gmail.com>
2021-12-07 05:37:40 -0500

    script: Set git_tag="GCC4.7_alpha_4" in download-gcc4.7-packages.sh

--- download-gcc4.7-packages.sh
+++ download-gcc4.7-packages.sh
@@ -1,22 +1,12 @@
 _download_packages() {
-  local git_tag="GCC4.7_alpha_2"
-  local gcc_number="4.7"
-  local gcc_pkgver="4.7.4"
-  local gcc_pkgrel="2"
-  local crt_number="3"
-  local crt_pkgver="3.4.0.3935.8c7e00886"
-  local crt_pkgrel="1"
-  wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/${git_tag}/mingw-w64-i686-crt${crt_number}-git-${crt_pkgver}-${crt_pkgrel}-any.pkg.tar.zst
-  wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/${git_tag}/mingw-w64-i686-gcc${gcc_number}-${gcc_pkgver}-${gcc_pkgrel}-any.pkg.tar.zst
-  wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/${git_tag}/mingw-w64-i686-gcc${gcc_number}-libs-${gcc_pkgver}-${gcc_pkgrel}-any.pkg.tar.zst
-  wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/${git_tag}/mingw-w64-i686-headers${crt_number}-git-${crt_pkgver}-${crt_pkgrel}-any.pkg.tar.zst
-  wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/${git_tag}/mingw-w64-i686-pthread-stub-headers${crt_number}-git-${crt_pkgver}-${crt_pkgrel}-any.pkg.tar.zst
-  wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/${git_tag}/mingw-w64-x86_64-crt${crt_number}-git-${crt_pkgver}-${crt_pkgrel}-any.pkg.tar.zst
-  wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/${git_tag}/mingw-w64-x86_64-gcc${gcc_number}-${gcc_pkgver}-${gcc_pkgrel}-any.pkg.tar.zst
-  wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/${git_tag}/mingw-w64-x86_64-gcc${gcc_number}-libs-${gcc_pkgver}-${gcc_pkgrel}-any.pkg.tar.zst
-  wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/${git_tag}/mingw-w64-x86_64-headers${crt_number}-git-${crt_pkgver}-${crt_pkgrel}-any.pkg.tar.zst
-  wget -nc https://github.com/stahta01/old-MINGW-packages/releases/download/${git_tag}/mingw-w64-x86_64-pthread-stub-headers${crt_number}-git-${crt_pkgver}-${crt_pkgrel}-any.pkg.tar.zst
+  local git_tag="GCC4.7_alpha_4"
+
+  wget -nc https://github.com/stahta01/opt-MINGW-packages/releases/download/${git_tag}/32bit.zip && \
+  wget -nc https://github.com/stahta01/opt-MINGW-packages/releases/download/${git_tag}/64bit.zip && \
+  7za e 32bit.zip && \
+  7za e 64bit.zip && \
+  rm -fr 32bit.zip && \
+  rm -fr 64bit.zip
 }
 
-mkdir -p packages && cd packages
-_download_packages
+mkdir -p packages && cd packages && _download_packages
