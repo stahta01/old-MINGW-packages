f61e6383f85ddac600c99a999be39337896326ca
Tim Stahlhut <stahta01@gmail.com>
2021-12-12 12:54:10 -0500

    script: Use 4.5-static GCC in copy-packages.sh

--- copy-packages.sh
+++ copy-packages.sh
@@ -2,6 +2,6 @@ mkdir -p packages/32bit && mkdir -p packages/64bit && \
 cd mingw-w64-crt3 && \
 cp mingw-w64-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cp mingw-w64-x86_64-*.pkg.tar.zst ../packages/64bit/ && \
-cd ../mingw-w64-gcc4.6 && \
+cd ../mingw-w64-gcc4.5-static && \
 cp mingw-w64-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cp mingw-w64-x86_64-*.pkg.tar.zst ../packages/64bit/
